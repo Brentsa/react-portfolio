c96deb8e175e7f3b4b85ed992e82098a57ce1912
Solidshots <seanbrent5@gmail.com>
2021-07-11 12:15:33 -0400

    react added

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className='flex-column justify-flex-start min-100-vh'>
+        <div className='container'>
+            Hello World!
+        </div>
+    </div>
+  );
+
+}
+
+export default App;
