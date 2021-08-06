d5dede73abd04c339654b79a931006655de60462
Solidshots <seanbrent5@gmail.com>
2021-08-06 19:39:55 -0400

    refactoring resume section

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,32 +1,40 @@
 //import the pdf from the assets so that we can set it as a download
 import pdf from '../assets/Resume.pdf'
+import { useState } from 'react';
 
 function Resume(){
     
+    const [languages] = useState(['Javascript', 'C++', 'Python', "SQL"]);
+    const [frontEnd] = useState(['React', 'Redux', 'Handlebars', 'HTML', 'CSS', "Bootstrap", "Material-UI", 'IndexedDB', 'Webpack'])
+    const [backEnd] = useState(['Express', 'MongoDB', 'MySql', 'Mongoose', 'GraphQL', 'Sequelize', 'Apollo', 'JSON Web Token', 'Express-Session'])
+    const [technologies] = useState([ 'Unreal Engine 4', 'Microsoft Visual Studio 2019', 'Microsoft Visual Code', 'Simple DirectMedia Layer 2.0 (SDL 2.0)'])
+
     //include a link to download Sean's resume
     return(
-        <section className="d-flex flex-column align-items-center color-dark my-4">
+        <section className="d-flex flex-column align-items-center col-12 color-dark my-4">
             <a href={pdf} download="Sean's Resume.pdf" className="a2 mb-4 pb-2 bottom-border"><h2>Download my resume here!</h2></a>
-            <div className="my-4">
-                <h3>Front-End Proficiencies</h3>
+            <div className="my-4 col-6 d-flex flex-column align-items-center">
+                <h3>Programming Languages</h3>
                 <ul>
-                    <li>React</li>
-                    <li>HandleBars</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>Javascript</li>
-                    <li>Bootstrap</li>
+                    {languages.map(language => <li>{language}</li>)}
                 </ul>
             </div>
-            <div className="my-4">
+            <div className="my-4 col-6 d-flex flex-column align-items-center">
+                <h3>Front-End Proficiencies</h3>
+                <ul>
+                    {frontEnd.map(tech => <li>{tech}</li>)}
+                </ul>
+            </div>
+            <div className="my-4 col-6 d-flex flex-column align-items-center">
                 <h3>Back-End Proficiencies</h3>
                 <ul>
-                    <li>Express</li>
-                    <li>MongoDB</li>
-                    <li>MySql</li>
-                    <li>Mongoose</li>
-                    <li>GraphQL</li>
-                    <li>Sequelize</li>
+                    {backEnd.map(tech => <li>{tech}</li>)}
+                </ul>
+            </div>
+            <div className="my-4 col-6 d-flex flex-column align-items-center">
+                <h3>Other Technologies</h3>
+                <ul>
+                    {technologies.map(tech => <li>{tech}</li>)}
                 </ul>
             </div>
         </section>
