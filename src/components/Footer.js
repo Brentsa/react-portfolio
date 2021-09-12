47f29e41ec08e55de43f5d717db1f9064162d548
Solidshots <seanbrent5@gmail.com>
2021-09-12 16:50:16 -0400

    slight style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,10 @@ function Footer(){
         <footer className="d-flex flex-column container justify-content-evenly background-primary color-tertiary">
             <div className="d-flex justify-content-evenly p-2">
                 <h1>
-                    <a className="svg-img a-light" href="https://stackoverflow.com/users/14084009/sean-brent" target="_blank" rel="noreferrer">
-                        <i className="fab fa-stack-overflow"></i>
-                    </a>
+                    <a className="svg-img a-light" href="tel:905-467-6136"><i className="fas fa-phone"></i></a>
+                </h1>
+                <h1>
+                    <a className="svg-img a-light" href="mailto: seanbrent5@gmail.com"><i className="fas fa-envelope"></i></a>
                 </h1>
                 <h1>
                     <a className="svg-img a-light" href="https://github.com/Brentsa" target="_blank" rel="noreferrer">
@@ -17,12 +18,11 @@ function Footer(){
                     <a className="svg-img a-light" href="https://www.linkedin.com/in/sean-brent/" target="_blank" rel="noreferrer">
                         <i className="fab fa-linkedin"></i>
                     </a>
-                </h1>
+                </h1> 
                 <h1>
-                    <a className="svg-img a-light" href="tel:905-467-6136"><i className="fas fa-phone"></i></a>
-                </h1>
-                <h1>
-                    <a className="svg-img a-light" href="mailto: seanbrent5@gmail.com"><i className="fas fa-envelope"></i></a>
+                    <a className="svg-img a-light" href="https://stackoverflow.com/users/14084009/sean-brent" target="_blank" rel="noreferrer">
+                        <i className="fab fa-stack-overflow"></i>
+                    </a>
                 </h1>
             </div>
         </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({currentPage, setPage, menuItems})
     return (
         <header className="container background-primary color-tertiary">
             <div className="row d-flex justify-content-around align-items-center">
-                <h1 className="col-12 col-md-6 text-center text-md-start my-2">Sean Brent: Developer</h1>
+                <h1 className="col-12 col-md-6 text-center text-md-start my-2">Sean Brent - Developer</h1>
                 <Navigation
                     currentPage={currentPage}
                     setPage={setPage}
