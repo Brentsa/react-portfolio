24ac04fa53d28e05c5bbcfe2693c66c7c794bdaa
Solidshots <seanbrent5@gmail.com>
2021-09-18 19:25:49 -0400

    footer revision to email and phone

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,15 @@
 
 function Footer(){
     return(
-        <footer className="d-flex flex-column container justify-content-evenly background-primary color-tertiary">
-            <div className="d-flex justify-content-evenly p-2">
-                <h1>
-                    <a className="svg-img a-light" href="tel:905-467-6136"><i className="fas fa-phone"></i></a>
-                </h1>
-                <h1>
-                    <a className="svg-img a-light" href="mailto: seanbrent5@gmail.com"><i className="fas fa-envelope"></i></a>
-                </h1>
-                <h1>
-                    <a className="svg-img a-light" href="https://github.com/Brentsa" target="_blank" rel="noreferrer">
-                        <i className="fab fa-github"></i>
-                    </a>
-                </h1>
-                <h1>
-                    <a className="svg-img a-light" href="https://www.linkedin.com/in/sean-brent/" target="_blank" rel="noreferrer">
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                </h1> 
-                <h1>
-                    <a className="svg-img a-light" href="https://stackoverflow.com/users/14084009/sean-brent" target="_blank" rel="noreferrer">
-                        <i className="fab fa-stack-overflow"></i>
-                    </a>
-                </h1>
+        <footer className="container background-primary color-tertiary">
+            <div className="row p-2 justify-content-center align-items-center">
+                <a className="svg-img a-light col-8 col-lg-3 text-lg-center text-nowrap p-2" href="tel:905-467-6136"><i className="fas fa-phone align-middle px-3"></i>905 467 6136</a>
+                <a className="svg-img a-light col-8 col-lg-3 text-lg-center text-nowrap p-2" href="mailto: seanbrent5@gmail.com"><i className="fas fa-envelope align-middle px-3"></i>seanbrent5@gmail.com</a> 
+                <div className=" col-12 col-lg-6 p-2 d-flex justify-content-evenly">
+                    <a className="svg-img a-light" href="https://github.com/Brentsa" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+                    <a className="svg-img a-light" href="https://www.linkedin.com/in/sean-brent/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a> 
+                    <a className="svg-img a-light" href="https://stackoverflow.com/users/14084009/sean-brent" target="_blank" rel="noreferrer"><i className="fab fa-stack-overflow"></i></a> 
+                </div>
             </div>
         </footer>
     )
